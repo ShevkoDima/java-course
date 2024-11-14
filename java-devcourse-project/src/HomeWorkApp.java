99a5a0123f67da8963b6e5334fc412a5260b3a1b
ShevkoDima <51541564+ShevkoDima@users.noreply.github.com>
2024-11-14 19:08:33 +0100

    Update HomeWorkApp.java

--- java-devcourse-project/src/HomeWorkApp.java
+++ java-devcourse-project/src/HomeWorkApp.java
@@ -84,10 +84,6 @@ public class HomeWorkApp {
     }
 
     static boolean isLeapYear(int year) {
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else return year % 4 == 0;
+        return year % 400 == 0 || year % 4 == 0 && !(year % 100 == 0);
     }
 }
